3ae076a99b7782403832819fc25f3c049a97ac08
angelazhou32 <angela.zhou@temporal.io>
2023-10-17 14:19:57 -0400

    removes structs-for-data exercises, adds the solution to a sample, removes versioning info out

--- samples/using-structs-for-data/start/main.go
+++ samples/using-structs-for-data/start/main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
-	translation "temporal102/exercises/using-structs/practice"
+	translation "temporal102/samples/using-structs-for-data"
 
 	"go.temporal.io/sdk/client"
 )
