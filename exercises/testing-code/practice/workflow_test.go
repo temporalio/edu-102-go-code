8d8c1923d51c4f5159dc4f289f8f880f0ccdfbc6
Brian Jing <153048309+jingb-anz@users.noreply.github.com>
2024-01-09 19:25:09 +1100

    Add code for workflow test

--- exercises/testing-code/practice/workflow_test.go
+++ exercises/testing-code/practice/workflow_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 
 	//	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"go.temporal.io/sdk/testsuite"
 )
 
-func IGNORETestSuccessfulCompleteFrenchTranslation(t *testing.T) {
+func TestSuccessfulCompleteFrenchTranslation(t *testing.T) {
 	s := testsuite.WorkflowTestSuite{}
 
 	env := s.NewTestWorkflowEnvironment()
@@ -20,14 +21,17 @@ func IGNORETestSuccessfulCompleteFrenchTranslation(t *testing.T) {
 
 	env.ExecuteWorkflow(SayHelloGoodbye, workflowInput)
 
-	// TODO: Assert that Workflow Execution completed
+	// Assert that Workflow Execution completed
+	assert.True(t, env.IsWorkflowCompleted())
 
 	var result TranslationWorkflowOutput
 	env.GetWorkflowResult(&result)
 
 	// TODO: Assert that the HelloMessage field in the
 	//       result is: Bonjour, Pierre
+	assert.Equal(t, "Bonjour, Pierre", result.HelloMessage)
 
 	// TODO: Assert that the GoodbyeMessage field in the
 	//       result is: Au revoir, Pierre
+	assert.Equal(t, "Au revoir, Pierre", result.GoodbyeMessage)
 }
