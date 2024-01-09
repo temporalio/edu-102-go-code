ec11eb2e6dde32681c46f9339588b899ed3233b8
Brian Jing <153048309+jingb-anz@users.noreply.github.com>
2024-01-09 19:25:02 +1100

    Add code for activity test

--- exercises/testing-code/practice/activity_test.go
+++ exercises/testing-code/practice/activity_test.go
@@ -1,11 +1,11 @@
 package translation
 
 import (
-	//	"errors"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	//	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/testsuite"
 )
 
@@ -25,4 +25,46 @@ func TestSuccessfulTranslateActivityHelloGerman(t *testing.T) {
 	var output TranslationActivityOutput
 	assert.NoError(t, future.Get(&output))
 	assert.Equal(t, "Hallo", output.Translation)
+}
+
+func TestSuccessfulTranslateActivityGoodbyeLatvian(t *testing.T) {
+	testSuite := testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestActivityEnvironment()
+	env.RegisterActivity(TranslateTerm)
+
+	input := TranslationActivityInput{
+		Term:         "Goodbye",
+		LanguageCode: "lv",
+	}
+
+	future, err := env.ExecuteActivity(TranslateTerm, input)
+	assert.NoError(t, err)
+
+	var output TranslationActivityOutput
+	assert.NoError(t, future.Get(&output))
+	assert.Equal(t, "Ardievu", output.Translation)
+}
+
+func TestFailedTranslateActivityBadLanguageCode(t *testing.T) {
+	testSuite := testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestActivityEnvironment()
+	env.RegisterActivity(TranslateTerm)
+
+	input := TranslationActivityInput{
+		Term:         "Hello",
+		LanguageCode: "xq",
+	}
+
+	_, err := env.ExecuteActivity(TranslateTerm, input)
+
+	// Assert that execution returned an error
+	assert.Error(t, err)
+
+	// Assert that this is a Temporal Application Error
+	var applicationErr *temporal.ApplicationError
+	assert.True(t, errors.As(err, &applicationErr))
+
+	// Assert that the error has the expected message, which identifies
+	// the invalid language code as the cause
+	assert.Equal(t, "HTTP Error 400: Unknown language code 'xq'\n", applicationErr.Message())
 }
