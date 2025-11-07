1c11c9efd5d39ee23d66caa0dd53fafb3c1bdf0d
Tom Wheeler <tom@temporal.io>
2025-11-07 14:11:11 -0600

    updated to reflect older ages returned by estimation service

--- samples/age-estimation/workflow_test.go
+++ samples/age-estimation/workflow_test.go
@@ -20,6 +20,6 @@ func Test_EstimateAge_EndToEnd(t *testing.T) {
 
 	var result string
 	assert.NoError(t, env.GetWorkflowResult(&result))
-	expected := "Betty has an estimated age of 77"
+	expected := "Betty has an estimated age of 78"
 	assert.Equal(t, expected, result)
 }
--- samples/age-estimation/workflow_with_mock_activity_test.go
+++ samples/age-estimation/workflow_with_mock_activity_test.go
@@ -13,14 +13,14 @@ func Test_EstimateAge_WithMockActivity(t *testing.T) {
 	env := testSuite.NewTestWorkflowEnvironment()
 
 	// This mock represents the RetrieveEstimate Activity, and if 
-	// passed "Betty" as input, it will return 77 as output.
-	env.OnActivity(RetrieveEstimate, mock.Anything, "Betty").Return(77, nil)
+	// passed "Betty" as input, it will return 78 as output.
+	env.OnActivity(RetrieveEstimate, mock.Anything, "Betty").Return(78, nil)
 
 	env.ExecuteWorkflow(EstimateAge, "Betty")
 	assert.True(t, env.IsWorkflowCompleted())
 
 	var result string
 	assert.NoError(t, env.GetWorkflowResult(&result))
-	expected := "Betty has an estimated age of 77"
+	expected := "Betty has an estimated age of 78"
 	assert.Equal(t, expected, result)
 }
